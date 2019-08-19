e94626e07132c41b64b8db077811561eea2740a8
Anand <anand.shenoy14@gmail.com>
2019-08-18 23:45:05 -0700

    main admin project first push

--- bgk-temple-admin-webapp/src/App.js
+++ bgk-temple-admin-webapp/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import SignIn from './SignIn'
+import MainAppBar from './MainAppBar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <MainAppBar></MainAppBar>
+        <SignIn></SignIn>
     </div>
   );
 }
